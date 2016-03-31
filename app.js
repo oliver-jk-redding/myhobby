cff3e8b22b8ce182c527fe47d05609a420d54c5d
oredds <oliver.jk.redding@gmail.com>
2016-03-31 18:41:05 +1300

    attempt at calling api

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 'use strict'
 
-require('dotenv').config()
+// require('dotenv').config()
 
 // Set default node environment to development
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+// process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 var express        = require('express') // import express.js
 var hbs            = require('express-hbs') // handlebars
@@ -11,28 +11,28 @@ var bodyParser     = require('body-parser') // parse request bodies
 var cookieParser   = require('cookie-parser')
 var path           = require('path') // work with file paths
 var logger 		   = require('morgan')
-var methodOverride = require('method-override') // allow put, delete through post
+// var methodOverride = require('method-override') // allow put, delete through post
 var request 	   = require('request')
 
 var routes = require('./routes')
 
 var app = express() // create the express application
-var server = require('http').createServer(app) // create the server
+// var server = require('http').createServer(app) // create the server
 
-app.engine('hbs', hbs.express4({
-  partialsDir: __dirname + '/views',
-  defaultLayout: __dirname + '/views/layout'
-}))
+// app.engine('hbs', hbs.express4({
+//   partialsDir: __dirname + '/views',
+//   defaultLayout: __dirname + '/views/layout'
+// }))
 
-app.set('view engine', 'hbs')
-app.set('views', __dirname + '/views')
+// app.set('view engine', 'hbs')
+// app.set('views', __dirname + '/views')
 
 app.use(logger( 'dev' ))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(methodOverride('_method'))
+// app.use(express.static(path.join(__dirname, 'public')))
+// app.use(methodOverride('_method'))
 
 app.get('/', function (req, res) {
   res.redirect('/myhobby/user')
@@ -40,52 +40,21 @@ app.get('/', function (req, res) {
 
 app.use('/myhobby', routes)
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
 request('/users', function(err, res, body) {
 	if(err || res.statusCode != 200) {console.log(err); return}
+	res.json(body)
 })
 
 
 
 
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
 // Start the app only when run with npm start
 // Don't run it when imported into the tests
-if (require.main === module) {
-  server.listen(5000, function () {
-    console.log('Running on port 5000!')
-  })
-}
+// if (require.main === module) {
+//   server.listen(5000, function () {
+//     console.log('Running on port 5000!')
+//   })
+// }
 
 // For testing purposes
 exports = module.exports = app
--- routes.js
+++ routes.js
@@ -1,59 +1,74 @@
 var express = require('express')
 var router = express.Router()
+var fs = require('fs')
+
+function getUserInfo(callback) {
+  fs.readFile('/./db/userInfo.json', function(err, data) {
+    if(err) {callback(err); return}
+    callback(data)
+  })
+}
 
 // Returns json of users
-router.get('/users', function (req, res) {
-  res.send(userData)
+router.get('/users', function (req, res, callback) {
+  // getUserInfo(function(err, data) {
+    // if(err) {callback(err); return}
+    res.status(200)
+    var json = {cats: [{name: 'piet'}]};
+    // res.json({cats: [{name: 'piet'}]});
+    // res.json(json)
+    callback(null, res, json)
+  // })
 })
 
 // Returns json of user information
 router.get('/user', function (req, res) {
-  res.send(userData)
+  // res.send(userData)
 })
 
 //returns a json of the user's hobbies
 router.get('/user/hobbies/', function (req, res) {
-  res.send(userProjectData)
+  // res.send(userProjectData)
 })
 
 // Returns json list of user's projects
 router.get('/user/projects/', function (req, res) {
-  res.send(userProjectData)
+  // res.send(userProjectData)
 })
 
 //returns a json of the id specified user's pinterest board
 router.get('/user/pinterest/:id', function (req, res) {
-  res.send(userProjectData)
+  // res.send(userProjectData)
 })
 
 //returns a json of the latest projects
 router.get('/projects/', function (req, res) {
-  res.send(userProjectData)
+  // res.send(userProjectData)
 })
 
 //returns a json of the latest projects of a specified hobby
 router.get('/projects/:hobby', function (req, res) {
-  res.send(userProjectData)
+  // res.send(userProjectData)
 })
 
 //returns a json of a project matching the specified id
 router.get('/projects/:id', function (req, res) {
-  res.send(userProjectData)
+  // res.send(userProjectData)
 })
 
 //creates a new project with the given id
 router.post('/projects/:id', function (req, res) {
-  res.send(userProjectData)
+  // res.send(userProjectData)
 })
 
 //returns a json of the updated project
 router.patch('/projects/:id', function (req, res) {
-  res.send()
+  // res.send()
 })
 
 //removes the project with the matching id and returns a json of the projects without the id
 router.delete('/projects/:id', function (req, res) {
-  res.end('You found the delete!')
+  // res.end('You found the delete!')
 })
 
 
