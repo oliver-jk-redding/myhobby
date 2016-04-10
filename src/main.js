badd05a5f83041efd245a996be73ae7d3786d095
oredds <oliver.jk.redding@gmail.com>
2016-04-10 17:13:37 +1200

    complete further profile rendering

--- src/main.js
+++ src/main.js
@@ -1,17 +1,27 @@
 var request = require('superagent');
-var newElem = require('./utils/make-new-element-util.js')
-var nav = require('./../views/nav.jade');
-var body = require('./../views/body.jade');
-var renderProfile = require('./utils/render-profile-util.js')
+
+// require templates
+var navTemplate = require('./../views/nav.jade');
+var profileTemplate = require('./../views/profile.jade');
+var contentTemplate = require('./../views/content.jade');
+
+// require utils
+var newElem = require('./utils/make-new-element-util.js');
+var renderProfile = require('./utils/render-profile-util.js');
+var renderProjects = require('./utils/render-projects-util.js');
 
 document.addEventListener("DOMContentLoaded", function() {
 
   request.get('http://localhost:3000/v1/myhobby/user/1', function(err, res) {
     if(res.status >= 200 && res.status < 400 && !err) {
       var userData = res.body;
-      document.body.appendChild(newElem('div', nav, userData));
-      document.body.appendChild(newElem('div', body));
+      document.body.appendChild(newElem('div', 'nav-wrapper', null, navTemplate, userData));
+      document.body.appendChild(newElem('div', 'profile-wrapper', null, profileTemplate));
+      document.body.appendChild(newElem('div', 'content-wrapper', null, contentTemplate));
       renderProfile(userData, function(element, callback) {
+        callback(element);
+      });
+      renderProjects(userData, function(element, callback) {
         callback(element);
       });
     }
--- src/utils/make-new-element-util.js
+++ src/utils/make-new-element-util.js
@@ -1,5 +1,9 @@
-module.exports = (elemType, template, data) => {
+module.exports = (elemType, idName, className, template, data) => {
   var elem = document.createElement(elemType);
+  if(idName)
+    elem.id = idName;
+  if(className)
+    elem.className += className;
   elem.innerHTML = template(data);
   return elem;
 }
